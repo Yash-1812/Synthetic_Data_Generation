998ee9a29c938502df1d998ce84fbf8b6ebee611
Deev82545 <f20240752@pilani.bits-pilani.ac.in>
2026-04-18 00:07:56 +0530

    Changes made in the ingestion file to round the data added and use KNNImputer to input data

--- synth_health/ingestion.py
+++ synth_health/ingestion.py
@@ -1,6 +1,11 @@
 import pandas as pd
+from sklearn.impute import KNNImputer
+import numpy as np
 
 class DataIngestion:
+    LOG_TRANSFORM_COLS = ['serum_creatinine', 'urine_albumin', 'albumin_creatinine_ratio',
+                      'blood_urea_nitrogen', 'uric_acid']
+    
     def __init__(self, file_path):
         self.file_path = file_path
         self.df = None
@@ -14,41 +19,71 @@ class DataIngestion:
     def infer_schema(self):
         """Infer column types"""
         schema = {}
-
+  
         for col in self.df.columns:
+            unique_ratio = self.df[col].nunique() / len(self.df)
             if pd.api.types.is_numeric_dtype(self.df[col]):
-                schema[col] = "numerical"
+                if unique_ratio < 0.0005:
+                    schema[col] = "categorical"
+                else:
+                    schema[col] = "numerical"
 
             elif pd.api.types.is_datetime64_any_dtype(self.df[col]):
                 schema[col] = "datetime"
 
             else:
-                # Heuristic: low unique values → categorical
-                unique_ratio = self.df[col].nunique() / len(self.df)
+                schema[col] = "text"
 
-                if unique_ratio < 0.05:
-                    schema[col] = "categorical"
-                else:
-                    schema[col] = "text"
 
         self.schema = schema
         return schema
 
+    def decimal_length(self, x):
+        s = str(x)
+        if '.' in s:
+        # Get the part after the decimal
+            decimal_part = s.split('.')[1]
+            decimal_part = decimal_part.rstrip('0') # Remove trailing zeros to fix the "1.0" Pandas conversion issue
+
+            return len(decimal_part)
+    # If no decimal point, the length is 0
+        return 0
+    
+
+    def rounding(self, col,mode_col):
+        amount_of_rounded_values = self.df[col].apply(self.decimal_length).dropna().unique().tolist()
+        #print(col,amount_of_rounded_values)
+        amount_of_rounded_values = [val for val in amount_of_rounded_values if val <= 2] 
+        if col in mode_col:
+            amount_of_rounded_values = [1]
+        #print(col,amount_of_rounded_values)
+        self.df[col] = [
+        round(val, np.random.choice(amount_of_rounded_values)) 
+        if pd.notna(val) else val 
+        for val in self.df[col]
+    ]
+        
     def handle_missing_values(self):
         """Basic missing value handling"""
-        for col, col_type in self.schema.items():
+        text_col = [col for col, t in self.schema.items() if t == "text"]
+        other_col = [col for col, t in self.schema.items() if t != "text" and t != "numerical"]
+        num_col = [col for col, t in self.schema.items() if t == "numerical"]
 
-            if col_type == "numerical":
-                self.df[col] = self.df[col].apply(lambda x: x if x > 1e-6 else pd.NA)
-                self.df[col] = self.df[col].fillna(self.df[col].mean())
+        imputer = KNNImputer(n_neighbors=20, weights="distance")
+        self.df[num_col] = imputer.fit_transform(self.df[num_col])
+        self.df[num_col] = self.df[num_col]
+        rounded_to_1 = ['albumin_serum','phosphorus','calcium']
 
-            elif col_type == "categorical":
-                self.df[col] = self.df[col].fillna(self.df[col].mode()[0])
-
-            else:
-                self.df[col] = self.df[col].fillna("unknown")
-
-        return self.df
+        for col in num_col:
+            self.rounding(col, rounded_to_1)
+        if 'ckd_stage' in self.df.columns:
+            mask = self.df['ckd_stage'].astype(str).str.lower() == 'unknown'
+            self.df.loc[mask, 'egfr'] = pd.NA
+        """for col in self.LOG_TRANSFORM_COLS:
+            if col in self.df.columns:
+                self.df[col] = np.log1p(self.df[col])"""
+        if 'age' in self.df.columns:
+            self.df['age'] = self.df['age'].clip(lower=1)
 
     def get_basic_stats(self):
         """Return basic statistics"""
@@ -70,12 +105,10 @@ class DataIngestion:
                 }
 
         return stats
-
+    
     def process(self):
-        """Run full pipeline"""
         self.load_data()
         self.infer_schema()
         self.handle_missing_values()
         stats = self.get_basic_stats()
-
         return self.df, self.schema, stats
--- synth_health/model.py
+++ synth_health/model.py
@@ -1,6 +1,10 @@
-from sdv.single_table import CTGANSynthesizer
+from sdv.single_table import CTGANSynthesizer, TVAESynthesizer
 from sdv.metadata import SingleTableMetadata
+from sdv.cag import Inequality, FixedCombinations
+import numpy as np
 
+LOG_TRANSFORM_COLS = ['serum_creatinine', 'urine_albumin', 'albumin_creatinine_ratio',
+                      'blood_urea_nitrogen', 'uric_acid']
 class SyntheticDataGenerator:
     def __init__(self, schema):
         self.schema = schema
@@ -11,13 +15,25 @@ class SyntheticDataGenerator:
         """Create metadata from dataframe"""
         metadata = SingleTableMetadata()
         metadata.detect_from_dataframe(df)
+
         return metadata
 
     def train(self, df):
         """Train CTGAN model"""
         self.metadata = self._create_metadata(df)
+        ckd_constraints = FixedCombinations(
+            column_names=['ckd_present', 'ckd_stage'])
+        diabetes_constraint = FixedCombinations(
+            column_names=['diabetes_diagnosed', 'insulin_use', 'diabetes_pills'])
+        smoker_constraint = FixedCombinations(
+            column_names=['ever_smoked', 'current_smoker'])
+        bp_constraint = Inequality(
+            low_column_name='bp_diastolic',
+            high_column_name='bp_systolic'
+        )
 
-        self.model = CTGANSynthesizer(self.metadata)
+        self.model = CTGANSynthesizer(self.metadata, epochs = 500, batch_size =500,enforce_rounding = True, enforce_min_max_values = True)
+        self.model.add_constraints(constraints=[bp_constraint, diabetes_constraint, smoker_constraint, ckd_constraints])
         self.model.fit(df)
 
     def generate(self, num_rows):
@@ -25,4 +41,9 @@ class SyntheticDataGenerator:
         if self.model is None:
             raise ValueError("Model not trained yet")
 
-        return self.model.sample(num_rows)
+        synthetic_df = self.model.sample(num_rows)
+        """ for col in LOG_TRANSFORM_COLS:
+            if col in synthetic_df.columns:
+                synthetic_df[col] = np.expm1(synthetic_df[col])"""
+
+        return synthetic_df
